3c897176229e00e23a6eff34776469b890cef268
illiadanko <illia.a.danko@gmail.com>
2025-01-28 19:09:31 +0200

    Add shfmt to format bash files

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -102,6 +102,7 @@
     pkgs.prismlauncher # minecraft launcher
     pkgs.rlwrap # wrap a command to make stdin interactive
     pkgs.sbcl
+    pkgs.shfmt # shell files formatter
     pkgs.slack
     pkgs.speechd # speech-dispatcher for foliate
     pkgs.stylua
