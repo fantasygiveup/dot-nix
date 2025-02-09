59a7f8fd5e8980b6508ae10e1a05ba54ee4dd244
illiadanko <illia.a.danko@gmail.com>
2025-02-09 17:57:10 +0100

    Revert "Opt-out emacs"
    This reverts commit d65e05c7552ca2e922c80f6395b0d3f7fd66d682.

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -50,6 +50,7 @@
     pkgs.discord
     pkgs.dive # inspect docker images
     pkgs.docker-compose
+    pkgs.emacs-gtk
     pkgs.emmet-ls
     pkgs.espeak # speach-module for speechd
     pkgs.filezilla
@@ -75,6 +76,8 @@
     pkgs.lf # terminal file manager
     pkgs.libnotify # provides notify-send
     pkgs.libreoffice-fresh # ms office, but better
+    pkgs.libtool # required by emacs
+    pkgs.libvterm # required by emacs
     pkgs.libxml2 # xmllint
     pkgs.lua-language-server
     pkgs.luajit # lua interpreter
